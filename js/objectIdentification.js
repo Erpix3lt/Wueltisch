f3781d266a621c542e0cb90b54bb3e56d5e15a25
mschmal2 <maximilian_dirk.schmalenbach@smail.th-koeln.de>
2020-12-27 19:09:59 +0100

    recent changes

--- js/objectIdentification.js
+++ js/objectIdentification.js
@@ -2,7 +2,7 @@
     // available on the index-page because of the script tag above.
   
     const img = document.getElementById('flying');
-    document.getElementById('result').innerHTML = 'loading';
+    document.getElementById('result').innerHTML = '[loading]';
     // Load the model.
     cocoSsd.load().then(model => {
       // detect objects in the image.
