1abdc88fbbbe26fecf4b8d60df7bcee0cd8ea245
mschmal2 <maximilian_dirk.schmalenbach@smail.th-koeln.de>
2021-04-16 13:11:48 +0200

    added bird videos

--- js/objectIdentification.js
+++ js/objectIdentification.js
@@ -1,22 +1,27 @@
-  // Notice there is no 'import' statement. 'cocoSsd' and 'tf' is
-    // available on the index-page because of the script tag above.
-  
-    const img = document.getElementById('flying');
-    document.getElementById('result').innerHTML = '[loading]';
-    // Load the model.
-    cocoSsd.load().then(model => {
-      // detect objects in the image.
-      model.detect(img).then(predictions => {
-        document.getElementById('result').innerHTML = predictions.map(a => a.class) + getShortenedScore(predictions);
-      });
+// Notice there is no 'import' statement. 'cocoSsd' and 'tf' is
+// available on the index-page because of the script tag above.
+
+function detectObjects() {
+  setInterval(function(){
+  const img = document.getElementById('flying');
+  document.getElementById('result').innerHTML = '[loading]';
+  // Load the model.
+  cocoSsd.load().then(model => {
+    // detect objects in the image.
+    model.detect(img).then(predictions => {
+      console.log(predictions);
+      document.getElementById('result').innerHTML = predictions.map(a => a.class) + getShortenedScore(predictions);
     });
+  });
+  }, 3000);
+}
 
-    function getShortenedScore(predictions){
-      let scoreArray = predictions.map(b => b.score);
-      //(6.688689).toFixed(3);
-      let shortenedScoreArray = scoreArray.map(function(element){
-        return element.toFixed(4);
-      });
+function getShortenedScore(predictions) {
+  let scoreArray = predictions.map(b => b.score);
+  //(6.688689).toFixed(3);
+  let shortenedScoreArray = scoreArray.map(function (element) {
+    return element.toFixed(4);
+  });
 
-      return shortenedScoreArray;
-    }
+  return shortenedScoreArray;
+}
